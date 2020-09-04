c0f8412a8528d874bf8f5e23b84142a5884760cc
minerva-b <64626133+minerva-b@users.noreply.github.com>
2020-09-03 19:02:43 -0700

    Setup filter search; not functional yet

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,6 +1,5 @@
-// Gather data from 'data.js':
+// Assign a variable and gather the data from 'data.js':
 var tableData = data;
-console.log(data);
 
 // Creating a reference to the table body:
 var tbody = d3.select("tbody");
@@ -10,7 +9,7 @@ var tbody = d3.select("tbody");
 // Within that loop, loop through each of the paired elements [key, value]:
 // Then append after each paired element to the table cells:
 // Show each entry into the table:
-data.forEach((UFOsightings) => {
+tableData.forEach((UFOsightings) => {
     var row = tbody.append("tr");
     Object.entries(UFOsightings).forEach(([key, value]) => {
       var cell = row.append("td");
@@ -18,3 +17,33 @@ data.forEach((UFOsightings) => {
     });
   });
 
+// Using a form so the user(s) can filter through the dates of UFO sightings:
+// Selecting the 'filter table' aka '#filter-btn' button:
+var button = d3.select("#filter-btn");
+
+// Selecting the '.form-control' form:
+var form = d3.select("#form");
+
+// Creating event handlers:
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Completing the event handler function for the form:
+function runEnter() {
+
+    // Halting the page from automatically refreshing:
+    d3.event.preventDefault();
+
+    // Selecting the input field element:
+    var inputElement = d3.select("#datetime");
+
+    // Getting the value property of the input field element:
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    console.log(tableData);
+
+    // Creating filter search:
+    var filteredData = tableData.filter(date => date.datetime === inputValue);
+    console.log(filteredData); 
+
+};
