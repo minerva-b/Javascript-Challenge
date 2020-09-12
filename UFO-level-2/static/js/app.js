3c9a91a682ee96962217913266c4341b5de3f801
minerva-b <64626133+minerva-b@users.noreply.github.com>
2020-09-12 14:55:26 -0700

    Each filter works now but it doesn't narrow down the search as it should

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -22,8 +22,6 @@ tableData.forEach((UFOsightings) => {
 var button = d3.select("#filter-btn");
 // Selecting the '.form-control' form:
 var form = d3.select("#form");
-// // Selecting the area for output results:
-// var results = d3.select("#searchResults");
 
 // Creating event handlers:
 button.on("click", runEnter);
@@ -37,38 +35,28 @@ function runEnter() {
     d3.event.preventDefault();
 
     // Selecting the input field elements:
-    var inputDate = d3.select("#searchDate");
-    // var inputCity = d3.select("#searchCity");
-    // var inputState = d3.select("#searchState");
-    // var inputCountry = d3.select("#searchCountry");
-    // var inputShape = d3.select("#searchShape");
+    var inputDate = d3.select("#datetime");
+    var inputCity = d3.select("#city");
+    var inputState = d3.select("#state");
+    var inputCountry = d3.select("#country");
+    var inputShape = d3.select("#shape");
 
     // Getting the value property of the input field elements:
     var inputDateValue = inputDate.property("value");
-    // console.log(inputDateValue);
-    // var inputCityValue = inputCity.property("value");
-    // // console.log(inputCityValue);
-    // var inputStateValue = inputState.property("value");
-    // // console.log(inputStateValue);
-    // var inputCountryValue = inputCountry.property("value");
-    // // console.log(inputCountryValue);
-    // var inputShapeValue = inputShape.property("value");
-    // // console.log(inputShapeValue);
+    var inputCityValue = inputCity.property("value");
+    var inputStateValue = inputState.property("value");
+    var inputCountryValue = inputCountry.property("value");
+    var inputShapeValue = inputShape.property("value");
 
     // Clearing the existing tableData from the page:
     tbody.html("");
 
     // Creating the filter search for 5 elements (date, city, state, country, and shape):
-    var filteredDate = tableData.filter(date => date.searchDate === inputDateValue);
-    // console.log(filteredDate);
-    // var filteredCity = tableData.filter(city => city.searchCity === inputCityValue);
-    // // console.log(filteredCity);
-    // var filteredState = tableData.filter(state => state.searchState === inputStateValue);
-    // // console.log(filteredState);
-    // var filteredCountry = tableData.filter(country => country.searchCountry === inputCountryValue);
-    // // console.log(filteredCountry);
-    // var filteredShape = tableData.filter(shape => shape.searchShape === inputShapeValue);
-    // // console.log(filteredShape);
+    var filteredDate = tableData.filter(searchDate => searchDate.datetime === inputDateValue);
+    var filteredCity = tableData.filter(searchCity => searchCity.city === inputCityValue);
+    var filteredState = tableData.filter(searchState => searchState.state === inputStateValue);
+    var filteredCountry = tableData.filter(searchCountry => searchCountry.country === inputCountryValue);
+    var filteredShape = tableData.filter(searchShape => searchShape.shape === inputShapeValue);
 
     // Loop through the filtered data based on user input elements:
     filteredDate.forEach((dateFilter) => {
@@ -78,32 +66,32 @@ function runEnter() {
           cell.text(value);
         });
       });
-    // filteredCity.forEach((cityFilter) => {
-    //   var row = tbody.append("tr");
-    //   Object.entries(cityFilter).forEach(([key, value]) => {
-    //     var cell = row.append("td");
-    //     cell.text(value);
-    //   });
-    // });
-    // filteredState.forEach((stateFilter) => {
-    //   var row = tbody.append("tr");
-    //   Object.entries(stateFilter).forEach(([key, value]) => {
-    //     var cell = row.append("td");
-    //     cell.text(value);
-    //   });
-    // });
-    // filteredCountry.forEach((countryFilter) => {
-    //   var row = tbody.append("tr");
-    //   Object.entries(countryFilter).forEach(([key, value]) => {
-    //     var cell = row.append("td");
-    //     cell.text(value);
-    //   });
-    // });
-    // filteredShape.forEach((shapeFilter) => {
-    //   var row = tbody.append("tr");
-    //   Object.entries(shapeFilter).forEach(([key, value]) => {
-    //     var cell = row.append("td");
-    //     cell.text(value);
-    //   });
-    // });
+    filteredCity.forEach((cityFilter) => {
+      var row = tbody.append("tr");
+      Object.entries(cityFilter).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+    filteredState.forEach((stateFilter) => {
+      var row = tbody.append("tr");
+      Object.entries(stateFilter).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+    filteredCountry.forEach((countryFilter) => {
+      var row = tbody.append("tr");
+      Object.entries(countryFilter).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+    filteredShape.forEach((shapeFilter) => {
+      var row = tbody.append("tr");
+      Object.entries(shapeFilter).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
 };
