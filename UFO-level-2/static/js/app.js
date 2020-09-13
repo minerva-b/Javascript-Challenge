eb2d51bbb7b34f905e771d46c21884528d01dfff
minerva-b <64626133+minerva-b@users.noreply.github.com>
2020-09-12 21:20:48 -0700

    All filters work but does not narrow down all at once; buttons finished

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -9,9 +9,9 @@ var tbody = d3.select("tbody");
 // Within that loop, loop through each of the paired elements [key, value]:
 // Then append after each paired element to the table cells:
 // Show each entry into the table:
-tableData.forEach((UFOsightings) => {
+tableData.map((UFOsightings) => {
     var row = tbody.append("tr");
-    Object.entries(UFOsightings).forEach(([key, value]) => {
+    Object.entries(UFOsightings).map(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
@@ -40,7 +40,7 @@ function runEnter() {
     var inputState = d3.select("#state");
     var inputCountry = d3.select("#country");
     var inputShape = d3.select("#shape");
-
+ 
     // Getting the value property of the input field elements:
     var inputDateValue = inputDate.property("value");
     var inputCityValue = inputCity.property("value");
@@ -59,35 +59,35 @@ function runEnter() {
     var filteredShape = tableData.filter(searchShape => searchShape.shape === inputShapeValue);
 
     // Loop through the filtered data based on user input elements:
-    filteredDate.forEach((dateFilter) => {
+    filteredDate.map((UFOsightings) => {
         var row = tbody.append("tr");
-        Object.entries(dateFilter).forEach(([key, value]) => {
+        Object.entries(UFOsightings).forEach(([key, value]) => {
           var cell = row.append("td");
           cell.text(value);
         });
       });
-    filteredCity.forEach((cityFilter) => {
+    filteredCity.map((cityFilter) => {
       var row = tbody.append("tr");
       Object.entries(cityFilter).forEach(([key, value]) => {
         var cell = row.append("td");
         cell.text(value);
       });
     });
-    filteredState.forEach((stateFilter) => {
+    filteredState.map((stateFilter) => {
       var row = tbody.append("tr");
       Object.entries(stateFilter).forEach(([key, value]) => {
         var cell = row.append("td");
         cell.text(value);
       });
     });
-    filteredCountry.forEach((countryFilter) => {
+    filteredCountry.map((countryFilter) => {
       var row = tbody.append("tr");
       Object.entries(countryFilter).forEach(([key, value]) => {
         var cell = row.append("td");
         cell.text(value);
       });
     });
-    filteredShape.forEach((shapeFilter) => {
+    filteredShape.map((shapeFilter) => {
       var row = tbody.append("tr");
       Object.entries(shapeFilter).forEach(([key, value]) => {
         var cell = row.append("td");
@@ -95,3 +95,7 @@ function runEnter() {
       });
     });
 };
+
+function refreshPage(){
+  window.location.reload();
+} 
